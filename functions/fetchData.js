d589d5d9e47b99699f56a0e381a4ce909724d06e
ayushpanwarvjf <saritapanwar706@gmail.com>
2025-09-26 13:37:12 +0530

    ctrl+s

---
+++ functions/fetchData.js
@@ -0,0 +1,37 @@
+// functions/fetchData.js
+const { exec } = require("child_process");
+
+exports.handler = async (event) => {
+  const number = event.queryStringParameters.number;
+
+    if (!number) {
+        return {
+              statusCode: 400,
+                    body: JSON.stringify({ error: "Missing ?number= parameter" }),
+                        };
+                          }
+
+                            return new Promise((resolve) => {
+                                const curlCmd = `curl -s "https://osintx.info/API/aetherdemo.php?key=SANJ33T&type=mobile&term=${number}"`;
+
+                                    exec(curlCmd, (error, stdout, stderr) => {
+                                          if (error) {
+                                                  resolve({
+                                                            statusCode: 500,
+                                                                      body: JSON.stringify({ error: error.message }),
+                                                                              });
+                                                                                    } else if (stderr) {
+                                                                                            resolve({
+                                                                                                      statusCode: 500,
+                                                                                                                body: JSON.stringify({ error: stderr }),
+                                                                                                                        });
+                                                                                                                              } else {
+                                                                                                                                      resolve({
+                                                                                                                                                statusCode: 200,
+                                                                                                                                                          headers: { "Content-Type": "application/json" },
+                                                                                                                                                                    body: stdout, // raw API JSON from curl
+                                                                                                                                                                            });
+                                                                                                                                                                                  }
+                                                                                                                                                                                      });
+                                                                                                                                                                                        });
+                                                                                                                                                                                        };
